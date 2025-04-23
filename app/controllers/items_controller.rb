fbd3c164314254b7f34b820ca128d427e955a55e
d-magaki <dkmk.03112600@gmail.com>
2025-04-23 10:32:54 +0900

    商品削除機能の修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :authenticate_user!, only: [:new, :create, :edit, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_user, only: [:edit, :destroy]
 
   def index
     @items = Item.order(created_at: :desc)
@@ -22,25 +23,7 @@ class ItemsController < ApplicationController
   def show
   end
 
-  def destroy
-    if current_user == @item.user
-      if @item.destroy
-        redirect_to root_path, notice: "商品を削除しました。"
-      else
-        redirect_to item_path(@item), alert: "商品を削除できませんでした。"
-      end
-    else
-      redirect_to root_path, alert: "削除権限がありません。"
-    end
-  end
-  
-  
-
   def edit
-    if current_user != @item.user
-      redirect_to root_path
-      return
-    end
   end
 
   def update
@@ -51,14 +34,27 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.destroy
+      redirect_to root_path, notice: "Item successfully deleted."
+    else
+      redirect_to item_path(@item), alert: "Failed to delete the item."
+    end
+  end
+
   private
 
   def set_item
     @item = Item.find(params[:id])
   end
 
+  def authorize_user
+    redirect_to root_path unless current_user == @item.user
+  end
+
   def item_params
-    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, :prefecture_id,
-                                 :shipping_day_id, :price, :image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :shipping_fee_id, 
+      :prefecture_id, :shipping_day_id, :price, :image
+    ).merge(user_id: current_user.id)
   end
 end
